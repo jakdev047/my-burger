c32581863000040813746ca15a245f44309e8c56
jakdev047 <jakfreelancer047@gmail.com>
2020-10-04 20:23:50 +0600

    Show Ingredients according to State

--- src/components/BurgerBuilder/Burger/Burger.js
+++ src/components/BurgerBuilder/Burger/Burger.js
@@ -2,14 +2,26 @@ import React, { Fragment } from 'react';
 import Ingredient from '../Ingredient/Ingredient';
 
 const Burger = props => {
+
+    let ingredientArr = props.ingredients.map(item => {
+        let amountArry = [...Array(item.amount).keys()];
+        return amountArry.map(_ => {
+            return <Ingredient key={Math.random()} type={item.type} />
+        });
+    }).reduce((arr,element)=>{
+        return arr.concat(element);
+    },[]);
+
+    if(ingredientArr.length === 0) {
+        ingredientArr = <p className="my-3" style={{color:'red',fontWeight:'bold'}}>Please add some Ingredients!</p>
+    }
+
     return (
-        <Fragment>
+        <div className="burger">
             <Ingredient type='bread-top' />
-            <Ingredient type='cheese' />
-            <Ingredient type='salad' />
-            <Ingredient type='meat' />
+            {ingredientArr}
             <Ingredient type='bread-bottom' />
-        </Fragment>
+        </div>
     )
 };
 
--- src/components/BurgerBuilder/BurgerBuilder.js
+++ src/components/BurgerBuilder/BurgerBuilder.js
@@ -2,13 +2,20 @@ import React, { Component, Fragment } from 'react';
 import Burger from './Burger/Burger';
 
 class BurgerBuilder extends Component {
+    state = {
+        ingredients: [
+            {type: 'salad', amount:0},
+            {type: 'cheese', amount:0},
+            {type: 'meat', amount:0},
+        ]
+    }
     render() {
         return (
             <Fragment>
                 <div className="container">
                     <div className="row">
                         <div className="col-lg-6">
-                            <Burger />
+                            <Burger ingredients={this.state.ingredients}/>
                         </div>
                     </div>
                 </div>
