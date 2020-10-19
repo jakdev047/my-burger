ddb4c638324cfd80ec0cf6975a35b8609bf66286
jakdev047 <jakfreelancer047@gmail.com>
2020-10-19 21:32:05 +0600

    Using Local Storage to Store token

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,47 +1,59 @@
-import React, { useState } from 'react';
+import React, { Component, useState } from 'react';
 import { connect } from 'react-redux';
 import { NavLink } from 'react-router-dom';
 import { Collapse, Nav, Navbar, NavbarBrand, NavbarToggler, NavItem } from 'reactstrap';
 
 import logo from '../../assets/images/logo.png';
+import { authCheck } from '../../redux/actions/auth';
 
-const Navigation = props => {
+class Navigation extends Component {
 
-    const [isOpen, setIsOpen] = useState(false);
+    state = {
+        isOpen: false,
+    }
 
-    const toggle = () => setIsOpen(!isOpen);
-    let links = null;
-    if (props.token === null) {
-        links = (
-            <Nav className="ml-auto" navbar>
-                <NavItem><NavLink exact to="/about">About</NavLink></NavItem>
-                <NavItem><NavLink exact to="/login">Login</NavLink></NavItem>
-            </Nav>
+    toggle = () => this.setState({
+        isOpen: !this.state.isOpen
+    });
+
+    componentDidMount(){
+        this.props.authCheck();
+    }
+
+    render() {
+        let links = null;
+        if (this.props.token === null) {
+            links = (
+                <Nav className="ml-auto" navbar>
+                    <NavItem><NavLink exact to="/about">About</NavLink></NavItem>
+                    <NavItem><NavLink exact to="/login">Login</NavLink></NavItem>
+                </Nav>
+            )
+        }
+        else {
+            links = (
+                <Nav className="ml-auto" navbar>
+                    <NavItem><NavLink exact to="/">Home</NavLink></NavItem>
+                    <NavItem><NavLink exact to="/orders">Orders</NavLink></NavItem>
+                    <NavItem><NavLink exact to="/checkout">Checkout</NavLink></NavItem>
+                    <NavItem><NavLink exact to="/about">About</NavLink></NavItem>
+                </Nav>
+            )
+        }
+        return (
+            <Navbar dark color="dark" expand="md">
+                <div className="container">
+                    <NavbarBrand href="/">
+                        <img src={logo} alt="burger-builder" />
+                    </NavbarBrand>
+                    <NavbarToggler onClick={this.toggle} />
+                    <Collapse isOpen={this.state.isOpen} navbar>
+                        {links}
+                    </Collapse>
+                </div>
+            </Navbar>
         )
     }
-    else {
-        links = (
-            <Nav className="ml-auto" navbar>
-                <NavItem><NavLink exact to="/">Home</NavLink></NavItem>
-                <NavItem><NavLink exact to="/orders">Orders</NavLink></NavItem>
-                <NavItem><NavLink exact to="/checkout">Checkout</NavLink></NavItem>
-                <NavItem><NavLink exact to="/about">About</NavLink></NavItem>
-            </Nav>
-        )
-    }
-    return (
-        <Navbar dark color="dark" expand="md">
-            <div className="container">
-                <NavbarBrand href="/">
-                    <img src={logo} alt="burger-builder" />
-                </NavbarBrand>
-                <NavbarToggler onClick={toggle} />
-                <Collapse isOpen={isOpen} navbar>
-                    {links}
-                </Collapse>
-            </div>
-        </Navbar>
-    )
 };
 const mapStateToProps = state => {
     return {
@@ -49,4 +61,11 @@ const mapStateToProps = state => {
         userId: state.auth.userId,
     }
 };
-export default connect(mapStateToProps)(Navigation);
+
+const mapDispatchToProps = dispatch => {
+    return {
+        authCheck: () => dispatch(authCheck())
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Navigation);
